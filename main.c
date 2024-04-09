762e3604a25e9003903ff7c021e93f31f7aa0da6
KonstantinosLamprakis <28678729+KonstantinosLamprakis@users.noreply.github.com>
2024-04-09 09:45:32 +0200

    Adding timestamp function

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: klamprak <klamprak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 03:13:26 by klamprak          #+#    #+#             */
-/*   Updated: 2024/04/09 08:48:58 by klamprak         ###   ########.fr       */
+/*   Updated: 2024/04/09 09:45:14 by klamprak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 // check for mem leaks
 // use safe functions ex. write instead of printf
 // check for errors wherever I have != 0
+// take care of case of single philo on table
 
 // https://www.geeksforgeeks.org/mutex-lock-for-linux-thread-synchronization/
 
@@ -26,9 +27,15 @@
 		and may have segm fault if use main's resources
 		- pthread_detach(): say that this thread has no longer nothing to do
 		with main, no resources, not accessible, it runs independently
- */
+
+	requipments:
+		- eat -> sleep -> think -> eat
+		- stops at die or eaten_num all of them
+		- message philo died < 10 ms
+*/
 
 void	*thread_routine(void *philo);
+long	get_timestamp(struct timeval tv_in);
 
 int	main(int argc, char **argv)
 {
@@ -55,8 +62,12 @@ int	main(int argc, char **argv)
 		philo_s[i].id = i;
 		philo_s[i].info = &info_s;
 		philo_s[i].state = 't';
-		pthread_create(&(philo_s[i].thread), NULL, thread_routine, (void *)&(philo_s[i]));
 	}
+	if (gettimeofday(&(info_s.tv_in), NULL) != 0)
+		return (0);
+	i = -1;
+	while (++i < info_s.phil_n)
+		pthread_create(&(philo_s[i].thread), NULL, thread_routine, (void *)&(philo_s[i]));
 	i = -1;
 	while (++i < info_s.phil_n)
 		pthread_join(philo_s[i].thread, NULL);
@@ -73,4 +84,15 @@ void	*thread_routine(void *philo)
 	printf("Thread [%ld]: Count at thread start = %d - %d\n", pthread_self(), philo_s->id, count++);
 	pthread_mutex_unlock(&(philo_s->info->print_m));
 	return (NULL);
+}
+
+long	get_timestamp(struct timeval tv_in)
+{
+	struct timeval	tv_cur;
+	long	timestamp;
+
+	if (gettimeofday(&tv_cur, NULL) != 0)
+		return (0);
+	timestamp = (tv_cur.tv_sec - tv_in.tv_sec) * 1000000 + tv_cur.tv_usec - tv_in.tv_usec;
+	return (timestamp);
 }
