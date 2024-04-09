d6bb87df0a99770f4e8faae1ea665097c9708e20
KonstantinosLamprakis <28678729+KonstantinosLamprakis@users.noreply.github.com>
2024-04-09 08:49:36 +0200

    Completed example on thread use, with mutex

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: klamprak <klamprak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 03:13:26 by klamprak          #+#    #+#             */
-/*   Updated: 2024/04/08 14:21:20 by klamprak         ###   ########.fr       */
+/*   Updated: 2024/04/09 08:48:58 by klamprak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,15 @@
 // check for errors wherever I have != 0
 
 // https://www.geeksforgeeks.org/mutex-lock-for-linux-thread-synchronization/
+
+/*
+	Info:
+		- pthread_join(): wait for a thread to return.
+		If you don't do it and main terminated, the thread continue to exist
+		and may have segm fault if use main's resources
+		- pthread_detach(): say that this thread has no longer nothing to do
+		with main, no resources, not accessible, it runs independently
+ */
 
 void	*thread_routine(void *philo);
 
@@ -32,7 +41,7 @@ int	main(int argc, char **argv)
 	info_s.forks = malloc (sizeof(pthread_mutex_t) * info_s.phil_n);
 	if (!info_s.forks)
 		return (1);
-	philo_s = malloc (sizeof(pthread_mutex_t) * info_s.phil_n);
+	philo_s = malloc (sizeof(t_philo) * info_s.phil_n);
 	if (!philo_s)
 		return (free(info_s.forks), 1);
 	pthread_mutex_init(&(info_s.print_m), NULL);
@@ -46,16 +55,22 @@ int	main(int argc, char **argv)
 		philo_s[i].id = i;
 		philo_s[i].info = &info_s;
 		philo_s[i].state = 't';
-		pthread_create(&(philo_s[i].thread), NULL, thread_routine, (void *)&philo_s[i]);
+		pthread_create(&(philo_s[i].thread), NULL, thread_routine, (void *)&(philo_s[i]));
 	}
+	i = -1;
+	while (++i < info_s.phil_n)
+		pthread_join(philo_s[i].thread, NULL);
+	return (0);
 }
 
 void	*thread_routine(void *philo)
 {
 	t_philo	*philo_s;
+	static int	count = 0;
 
 	philo_s = (t_philo*)philo;
 	pthread_mutex_lock(&(philo_s->info->print_m));
-	printf("Thread [%ld]: Count at thread start = %d\n", pthread_self(), philo_s->id);
+	printf("Thread [%ld]: Count at thread start = %d - %d\n", pthread_self(), philo_s->id, count++);
 	pthread_mutex_unlock(&(philo_s->info->print_m));
+	return (NULL);
 }
